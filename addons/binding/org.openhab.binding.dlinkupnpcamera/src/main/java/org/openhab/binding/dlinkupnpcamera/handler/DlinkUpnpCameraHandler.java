68c01e188a21f86cdcc2eafba07e7b2421035faf
lawchris <law.christopher38@gmail.com>
2017-04-01 22:27:42 +0200

    Handler and handlerFactory for DlinkUpnpCamera. Fixed bug about UpnpIOService being null.
    Also-by: Yacine Ndiaye <yacinenana@gmail.com> (github: yacinend)
    Also-by: Antoine Blanc <antoineblanc269@gmail.com> (github: AntoineBL)
    Signed-off-by: Christopher Law <law.christopher38@gmail.com> (github: lawchris)

--- addons/binding/org.openhab.binding.dlinkupnpcamera/src/main/java/org/openhab/binding/dlinkupnpcamera/handler/DlinkUpnpCameraHandler.java
+++ addons/binding/org.openhab.binding.dlinkupnpcamera/src/main/java/org/openhab/binding/dlinkupnpcamera/handler/DlinkUpnpCameraHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2016 by the respective copyright holders.
+ * Copyright (c) 2014-2017 by the respective copyright holders.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,51 +9,277 @@ package org.openhab.binding.dlinkupnpcamera.handler;
 
 import static org.openhab.binding.dlinkupnpcamera.DlinkUpnpCameraBindingConstants.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.smarthome.config.discovery.DiscoveryServiceRegistry;
+import org.eclipse.smarthome.core.library.types.NextPreviousType;
+import org.eclipse.smarthome.core.library.types.PlayPauseType;
+import org.eclipse.smarthome.core.library.types.StringType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
+import org.eclipse.smarthome.core.thing.ThingStatusDetail;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.io.transport.upnp.UpnpIOParticipant;
+import org.eclipse.smarthome.io.transport.upnp.UpnpIOService;
+import org.openhab.binding.dlinkupnpcamera.config.DlinkUpnpCameraConfiguration;
+import org.openhab.io.net.actions.Ping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * The {@link DlinkUpnpCameraHandler} is responsible for handling commands, which are
  * sent to one of the channels.
- * 
- * @author Law Christopher - Initial contribution
+ *
+ * @author Yacine Ndiaye
+ * @author Antoine Blanc
+ * @author Christopher Law
  */
-public class DlinkUpnpCameraHandler extends BaseThingHandler {
+public class DlinkUpnpCameraHandler extends BaseThingHandler implements UpnpIOParticipant {
+
+    private static final int DEFAULT_REFRESH_INTERVAL = 30;
+    private static final int PING_TIMEOUT = 20;
+
+    private String username;
+    private String password;
+    private String hostname;
 
     private Logger logger = LoggerFactory.getLogger(DlinkUpnpCameraHandler.class);
 
-	public DlinkUpnpCameraHandler(Thing thing) {
-		super(thing);
-	}
+    private UpnpIOService service;
+    private ScheduledFuture<?> pollingJob;
 
-	@Override
-	public void handleCommand(ChannelUID channelUID, Command command) {
-        if(channelUID.getId().equals(CHANNEL_1)) {
-            // TODO: handle command
+    private Runnable pollingRunnable = new Runnable() {
 
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
+        @Override
+        public void run() {
+            try {
+                logger.debug("Polling...");
+
+                // First check if the camera is set in the UPnP service registry
+                // If not, set the thing state to OFFLINE and wait for the next poll
+                if (!isUpnpDeviceRegistered()) {
+                    logger.debug("UPnP device {} not yet registered", getUDN());
+                    updateStatus(ThingStatus.OFFLINE);
+                    return;
+                }
+
+                // Check if the camera can be joined
+                // If not, set the thing state to OFFLINE and do nothing else
+                updatePlayerState();
+                if (getThing().getStatus() != ThingStatus.ONLINE) {
+                    return;
+                }
+            } catch (Exception e) {
+                logger.debug("Exception during poll : {}", e);
+            }
         }
-	}
+    };
+
+    private boolean isUpnpDeviceRegistered() {
+        return service.isRegistered(this);
+    }
+
+    public DlinkUpnpCameraHandler(Thing thing, UpnpIOService upnpIOService,
+            DiscoveryServiceRegistry discoveryServiceRegistry) {
+        super(thing);
+
+        logger.debug("Creating a DlinkUpnpCameraHandler for thing '{}'", getThing().getUID());
+        if (upnpIOService != null) {
+            logger.debug("Ok");
+            this.service = upnpIOService;
+        }
+    }
+
+    @Override
+    public String getUDN() {
+        return getConfigAs(DlinkUpnpCameraConfiguration.class).udn;
+    }
+
+    private void updatePlayerState() {
+
+        String udn = getThing().getProperties().get(DlinkUpnpCameraConfiguration.UDN);
+
+        try {
+            boolean isAlive = Ping.checkVitality(hostname, 0, PING_TIMEOUT);
+            logger.debug("isAlive value: {}", isAlive);
+            if (isAlive) {
+                if (!ThingStatus.ONLINE.equals(getThing().getStatus())) {
+                    logger.debug("Camera {} is connected to local network", getUDN());
+                    updateStatus(ThingStatus.ONLINE);
+                }
+            } else {
+                if (!ThingStatus.OFFLINE.equals(getThing().getStatus())) {
+                    logger.debug("Camera {} is disconnected from local network", getUDN());
+                    updateStatus(ThingStatus.OFFLINE);
+                }
+            }
+        } catch (IOException e) {
+            logger.debug("couldn't establish network connection [host '{}']", new Object[] { hostname });
+        }
+    }
+
+    @Override
+    public void handleCommand(ChannelUID channelUID, Command command) {
+        switch (channelUID.getId()) {
+            case USERNAME:
+                setUsername(command);
+                break;
+            case PASSWORD:
+                setPassword(command);
+                break;
+            case PAN:
+                try {
+                    sendControlCommand(command, 10, 0);
+                } catch (FileNotFoundException e) {
+                    logger.debug("Command not supported by the camera");
+                }
+                break;
+            case TILT:
+                try {
+                    sendControlCommand(command, 0, 10);
+                } catch (FileNotFoundException e) {
+                    logger.debug("Command not supported by the camera");
+                }
+                break;
+            default:
+                // Nothing is done
+        }
+    }
+
+    private void setPassword(Command command) {
+        if (command instanceof StringType) {
+            password = command.toString();
+            logger.debug("password set to {}", command.toString());
+        }
+    }
+
+    private void setUsername(Command command) {
+        if (command instanceof StringType) {
+            username = command.toString();
+            logger.debug("username set to {}", command.toString());
+        }
+    }
+
+    private void sendControlCommand(Command command, int x, int y) throws FileNotFoundException {
+        if (command instanceof NextPreviousType || command instanceof PlayPauseType) {
+            logger.debug("command {}", command.toString());
+            switch (command.toString()) {
+                case "NEXT":
+                    sendHttpRequest(buildCommandUrl(hostname, x, y));
+                    break;
+                case "PREVIOUS":
+                    sendHttpRequest(buildCommandUrl(hostname, -x, -y));
+                    break;
+                case "PLAY":
+                    sendHttpRequest(buildPatrolUrl(hostname));
+                    break;
+                case "PAUSE":
+                    sendHttpRequest(buildStopUrl(hostname));
+                    break;
+                default:
+                    // Nothing is done
+            }
+        }
+    }
 
     @Override
     public void initialize() {
-        // TODO: Initialize the thing. If done set status to ONLINE to indicate proper working.
-        // Long running initialization should be done asynchronously in background.
-        updateStatus(ThingStatus.ONLINE);
 
-        // Note: When initialization can NOT be done set the status with more details for further
-        // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work
-        // as expected. E.g.
-        // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
-        // "Can not access device as username and/or password are invalid");
+        logger.debug("initializing handler for thing {}", getThing().getUID());
+
+        if (getUDN() != null) {
+            updateStatus(ThingStatus.ONLINE);
+            onUpdate();
+            hostname = getThing().getProperties().get(DlinkUpnpCameraConfiguration.IP);
+            super.initialize();
+            logger.debug("Camera initialized.");
+        } else {
+            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
+            logger.warn("Cannot initalize the camera. UDN not set.");
+        }
+    }
+
+    private void onUpdate() {
+        if (pollingJob == null || pollingJob.isCancelled()) {
+            DlinkUpnpCameraConfiguration config = getConfigAs(DlinkUpnpCameraConfiguration.class);
+            // use default if not specified
+            int refreshInterval = DEFAULT_REFRESH_INTERVAL;
+            if (config.refresh != null) {
+                refreshInterval = config.refresh.intValue();
+            }
+            pollingJob = scheduler.scheduleWithFixedDelay(pollingRunnable, 0, refreshInterval, TimeUnit.SECONDS);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        logger.debug("Handler disposed for thing {}", getThing().getUID());
+
+        if (pollingJob != null && !pollingJob.isCancelled()) {
+            pollingJob.cancel(true);
+            pollingJob = null;
+        }
+    }
+
+    @Override
+    public void onValueReceived(String variable, String value, String service) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onServiceSubscribed(String service, boolean succeeded) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(boolean status) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     *
+     * @param string_url
+     */
+    private void sendHttpRequest(String string_url) {
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password.toCharArray());
+            }
+        });
+        URL url = null;
+        try {
+            url = new URL(String.format(string_url));
+            HttpURLConnection connection = null;
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream in = connection.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String buildCommandUrl(String hostname, int x, int y) {
+        return "http://" + hostname + "/cgi/ptdc.cgi?command=set_relative_pos&posX=" + x + "&posY=" + y;
+    }
+
+    private String buildPatrolUrl(String hostname) {
+        return "http://" + hostname + "/cgi/ptdc.cgi?command=pan_patrol";
+    }
+
+    private String buildStopUrl(String hostname) {
+        return "http://" + hostname + "/cgi/ptdc.cgi?command=stop";
     }
 }
--- addons/binding/org.openhab.binding.dlinkupnpcamera/src/main/java/org/openhab/binding/dlinkupnpcamera/internal/DlinkUpnpCameraHandlerFactory.java
+++ addons/binding/org.openhab.binding.dlinkupnpcamera/src/main/java/org/openhab/binding/dlinkupnpcamera/internal/DlinkUpnpCameraHandlerFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2016 by the respective copyright holders.
+ * Copyright (c) 2014-2017 by the respective copyright holders.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,42 +7,102 @@
  */
 package org.openhab.binding.dlinkupnpcamera.internal;
 
-import static org.openhab.binding.dlinkupnpcamera.DlinkUpnpCameraBindingConstants.*;
-
-import java.util.Collections;
-import java.util.Set;
-
-import org.openhab.binding.dlinkupnpcamera.handler.DlinkUpnpCameraHandler;
+import org.eclipse.smarthome.config.core.Configuration;
+import org.eclipse.smarthome.config.discovery.DiscoveryServiceRegistry;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.ThingUID;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
 import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.io.transport.upnp.UpnpIOService;
+import org.openhab.binding.dlinkupnpcamera.DlinkUpnpCameraBindingConstants;
+import org.openhab.binding.dlinkupnpcamera.config.DlinkUpnpCameraConfiguration;
+import org.openhab.binding.dlinkupnpcamera.handler.DlinkUpnpCameraHandler;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * The {@link DlinkUpnpCameraHandlerFactory} is responsible for creating things and thing 
+ * The {@link upnpcameraHandlerFactory} is responsible for creating things and thing
  * handlers.
- * 
- * @author Law Christopher - Initial contribution
+ *
+ * @author Yacine Ndiaye
+ * @author Antoine Blanc
+ * @author Christopher Law
  */
 public class DlinkUpnpCameraHandlerFactory extends BaseThingHandlerFactory {
-    
-    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
-    
+    private Logger logger = LoggerFactory.getLogger(DlinkUpnpCameraHandlerFactory.class);
+
+    private UpnpIOService upnpIOService;
+    private DiscoveryServiceRegistry discoveryServiceRegistry;
+
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+
+        return DlinkUpnpCameraBindingConstants.SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
     @Override
     protected ThingHandler createHandler(Thing thing) {
-
+        logger.debug("ThingHandler createHandler");
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.equals(THING_TYPE_SAMPLE)) {
-            return new DlinkUpnpCameraHandler(thing);
+        if (thingTypeUID.equals(DlinkUpnpCameraBindingConstants.CAMERA_THING_TYPE_UID)) {
+            return new DlinkUpnpCameraHandler(thing, upnpIOService, discoveryServiceRegistry);
         }
 
         return null;
     }
-}
 
+    @Override
+    protected void activate(ComponentContext componentContext) {
+        super.activate(componentContext);
+        logger.debug("activate upnpcameraHandler");
+    };
+
+    @Override
+    public Thing createThing(ThingTypeUID thingTypeUID, Configuration configuration, ThingUID thingUID,
+            ThingUID bridgeUID) {
+        if (DlinkUpnpCameraBindingConstants.CAMERA_THING_TYPE_UID.equals(thingTypeUID)) {
+            logger.debug("thingUID " + thingTypeUID);
+            logger.debug("thingUID " + thingUID);
+            ThingUID cameraUID = getCameraUID(thingTypeUID, thingUID, configuration);
+            logger.debug("Creating a Camera thing with ID '{}'", cameraUID);
+            Thing result = super.createThing(thingTypeUID, configuration, cameraUID, null);
+            if (result != null) {
+                logger.debug("Thing not null");
+            } else {
+                logger.debug("Thing null");
+            }
+            return result;
+        }
+        throw new IllegalArgumentException(
+                "The thing type " + thingTypeUID + " is not supported by the UpnpCamera binding.");
+    }
+
+    private ThingUID getCameraUID(ThingTypeUID thingTypeUID, ThingUID thingUID, Configuration configuration) {
+        String udn = (String) configuration.get(DlinkUpnpCameraConfiguration.UDN);
+
+        if (thingUID == null) {
+            thingUID = new ThingUID(thingTypeUID, udn);
+        }
+
+        return thingUID;
+    }
+
+    protected void setUpnpIOService(UpnpIOService upnpIOService) {
+        this.upnpIOService = upnpIOService;
+    }
+
+    protected void unsetUpnpIOService(UpnpIOService upnpIOService) {
+        this.upnpIOService = null;
+    }
+
+    protected void setDiscoveryServiceRegistry(DiscoveryServiceRegistry discoveryServiceRegistry) {
+        this.discoveryServiceRegistry = discoveryServiceRegistry;
+    }
+
+    protected void unsetDiscoveryServiceRegistry(DiscoveryServiceRegistry discoveryServiceRegistry) {
+        this.discoveryServiceRegistry = null;
+    }
+}
